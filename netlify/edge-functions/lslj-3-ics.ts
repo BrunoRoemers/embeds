d39b4862402dcb3b9a848b14c58403d658350535
Bruno <69008445+BrunoRoemers@users.noreply.github.com>
2023-09-30 18:22:51 +0200

    Serve ICS through edge function

--- netlify/edge-functions/lslj-3-ics.ts
+++ netlify/edge-functions/lslj-3-ics.ts
@@ -1,1 +1,7 @@
-export default () => new Response("Hello world");
+export default async () => {
+  const ics = await fetch(
+    "https://calendar.google.com/calendar/ical/6b678d7a1fcaa8cbc8c4c3ec341cf24b9aa002eaff96c05cc7a730ecc61b8b51%40group.calendar.google.com/public/basic.ics"
+  );
+
+  return new Response(await ics.blob());
+};
